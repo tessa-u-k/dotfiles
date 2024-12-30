6d49e4be2fff8dae714e751ba7389ec48336e9f6
tessa-u-k <lei@klbr.systems>
2024-12-30 12:44:55 -0500

    fix formatting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -34,5 +34,7 @@ return require('packer').startup(function(use)
     use({'neovim/nvim-lspconfig'})
     use({'hrsh7th/nvim-cmp'})
     use({'hrsh7th/cmp-nvim-lsp'})
-    use {'huggingface/llm.nvim'}
+
+    use({'huggingface/llm.nvim'})
+
 end)
