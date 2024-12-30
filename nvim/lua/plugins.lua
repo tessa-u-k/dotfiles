bd7039680007a484d5b98ebb28a8a2a7ca97691c
tessa-u-k <lei@klbr.systems>
2024-12-30 12:43:55 -0500

    add llm

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -34,4 +34,5 @@ return require('packer').startup(function(use)
     use({'neovim/nvim-lspconfig'})
     use({'hrsh7th/nvim-cmp'})
     use({'hrsh7th/cmp-nvim-lsp'})
+    use {'huggingface/llm.nvim'}
 end)
