a26afa8e4027b35427f4de2108fad5d93d0ac4f2
angel <angel@klbr.mom>
2025-11-01 02:35:12 -0400

    add nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,8 @@
   description = "Multi-platform Nix configuration";
 
   inputs = {
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
     lix = {
       url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
